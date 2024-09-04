b8b8e704a6e510a413e701a0e08377e99c521649
vyportfolio1 <vyportfolio1@gmail.com>
2024-09-04 12:47:34 +0300

    wop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,1 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        red: '#EA0E00',
-        black: '#0B0B0B',
-        white: '#FFFFFF',
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      },
-      screens: {
-        'lgL': '1440px', // The custom breakpoint
-      },
-    },
-  },
-}
+ 
